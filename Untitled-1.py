7bbd31fb80d11731c7b0f51a004b6402d671e8f1
Abdulazizov <asilbekabdulazizov>
2024-07-03 00:06:30 +0500

    code succesfully

---
+++ Untitled-1.py
@@ -0,0 +1,59 @@
+import psycopyg2 
+conn = psycopyg2.connect(database = 'n48',
+                        user = 'posgres',
+                        host = 'localhost',
+                        password = 'qazwsx',
+                        port = 5432
+                        )
+cursor = conn.cursor()
+
+create_foydalanuvchi_table = '''create_table_foydalanuvchi(
+      
+    id serial primary key ,
+    first_name varchar(100),
+    username varchar(100),
+    email varchar(100) not null,
+    age int,
+    is_active bool
+);
+'''
+cursor.execute(create_foydalanuvchi_table)
+conn.commit()
+
+class User:
+    def __init__(self,first_name,last_name,username,email,age,is_active) -> None:
+        self.first_name = first_name,
+        self.last_name = last_name,
+        self.username = username,
+        self.email = email,
+        self.age = age,
+        self.is_active = is_active
+    def save(self):
+        insert_into_query  = '''
+        inser into foydalanuvchi(firs_name,last_name,username,email,age,is_active)
+        values(%s,%s,%s,%s,%s,%s); 
+        ''' 
+        malumot = (self.firs_name,self.last_name,self.username,self.email,self.age,self.is_active)
+        cursor.execute(insert_into_query)
+        conn.commit()
+        
+    def delet_user(self):
+        delete_teble_del = '''
+        DELETE FROM foydalanuvchi WHERE id = 1;    
+        '''
+        cursor.execute(delete_teble_del)
+        conn.commit()
+        
+    def update_user(self):
+        update_table = '''
+         inser into foydalanuvchi(firs_name,last_name,username,email,age,is_active)
+        values(%s,%s,%s,%s,%s,%s);
+        '''
+        
+            
+update_table = User('Kevin','Bruno','Kevindebruno','kevindebruno17@gmail.com',32,True)
+update_table.update_user     
+user_male = User('john','Doe','johnDoe','joh@gmail.com',32,True)
+user_male.save
+user_female = User('Anna','Asti','anniasti','anna10@gmail.com',27,True)
+user_female.save
